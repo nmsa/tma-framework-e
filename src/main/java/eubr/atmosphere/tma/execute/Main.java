c3678fed0e71ca92afb831ea16a93018d24d6b86
Jose Pereira <josep@dei.uc.pt>
2018-10-02 14:47:31 +0100

    Performing an initial adapation through Execute

--- src/main/java/eubr/atmosphere/tma/execute/Main.java
+++ src/main/java/eubr/atmosphere/tma/execute/Main.java
@@ -8,13 +8,10 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.gson.Gson;
-
 import eubr.atmosphere.tma.data.Action;
 import eubr.atmosphere.tma.data.Actuator;
 import eubr.atmosphere.tma.execute.utils.PropertiesManager;
 import eubr.atmosphere.tma.execute.utils.RestServices;
-import eubr.atmosphere.tma.utils.Score;
 
 /**
  * Hello world!
@@ -31,13 +28,13 @@ public class Main
 
     private static void runConsumer() {
 
+        Action action = new Action("", 10);
+        act(obtainActuator(action), action);
+
         Consumer<Long, String> consumer = ConsumerCreator.createConsumer();
         int noMessageFound = 0;
         int maxNoMessageFoundCount = Integer.parseInt(
                 PropertiesManager.getInstance().getProperty("maxNoMessageFoundCount"));
-
-        Action action = new Action("", 10);
-        act(obtainActuator(action), action);
 
         try {
             while (true) {
@@ -90,15 +87,24 @@ public class Main
 
         // TODO: It needs to select the data from the database
         Actuator actuator = new Actuator();
-        actuator.setAddress("https://jsonplaceholder.typicode.com/posts");
+        actuator.setAddress("http://192.168.122.34:8089/apis/extensions/v1beta1/namespaces/default/deployments/tma-analyze/scale");
         actuator.setPubKey("my-key");
 
         return actuator;
     }
 
     private static void act(Actuator actuator, Action action) {
+        // Perform the adaptation (Part of it will be moved to the Actuator)
+        try {
+            RestServices.requestPutRestService(actuator, action);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
         // TODO: Request the REST service (actuator), with the definition from the Action
         LOGGER.info("ACTUATION TO BE IMPLEMENTED: " + actuator);
+        actuator.setAddress("https://jsonplaceholder.typicode.com/posts");
         try {
             RestServices.requestRestService(actuator, action);
         } catch (IOException e) {
--- src/main/java/eubr/atmosphere/tma/execute/utils/RestServices.java
+++ src/main/java/eubr/atmosphere/tma/execute/utils/RestServices.java
@@ -1,6 +1,7 @@
 package eubr.atmosphere.tma.execute.utils;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -13,7 +14,6 @@ public class RestServices {
 
     public static void requestRestService(Actuator actuator, Action action) throws IOException {
         // Reference: https://www.baeldung.com/java-http-request
-        
         URL url = new URL(actuator.getAddress());
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
@@ -30,7 +30,47 @@ public class RestServices {
           System.out.println(inputLine);
         }
         in.close();
-        
+
+        con.disconnect();
+    }
+
+    public static void requestPutRestService(Actuator actuator, Action action) throws IOException {
+        // FIXME It would be better to use PATCH instead of PUT.
+        //       However, the first experiments did not work, and we decided to move with PUT.
+        URL url = new URL(actuator.getAddress());
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("PUT");
+        con.setRequestProperty("Content-Type", "application/json");
+
+        con.setConnectTimeout(5000);
+        con.setReadTimeout(5000);
+
+        String newPayload = "{\n" +
+                "  \"metadata\": {\n" +
+                "    \"namespace\": \"default\",\n" +
+                "    \"name\": \"tma-analyze\"\n" +
+                "  },\n" +
+                "  \"spec\": {\n" +
+                "    \"replicas\": 4\n" +
+                "  }\n" +
+                "}";
+
+        con.setDoOutput(true);
+        DataOutputStream out = new DataOutputStream(con.getOutputStream());
+        out.writeBytes(newPayload);
+        out.flush();
+        out.close();
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+          content.append(inputLine);
+          System.out.println(inputLine);
+        }
+        in.close();
+
         con.disconnect();
     }
 }
