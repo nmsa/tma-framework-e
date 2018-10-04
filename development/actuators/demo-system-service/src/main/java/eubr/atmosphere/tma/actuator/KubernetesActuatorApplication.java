558df198d609e8796f39212a7f0b659806bb6c31
Jose Pereira <josep@dei.uc.pt>
2018-10-04 14:19:52 +0100

    Refactoring project name

--- development/actuators/demo-system-service/src/main/java/eubr/atmosphere/tma/actuator/KubernetesActuatorApplication.java
+++ development/actuators/demo-system-service/src/main/java/eubr/atmosphere/tma/actuator/KubernetesActuatorApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoSystemServiceApplication {
+public class KubernetesActuatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoSystemServiceApplication.class, args);
+		SpringApplication.run(KubernetesActuatorApplication.class, args);
 	}
 }
