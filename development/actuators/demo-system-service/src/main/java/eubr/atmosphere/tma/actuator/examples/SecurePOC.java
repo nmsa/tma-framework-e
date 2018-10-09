855a6fd470c1791200b9e3d1cbf9c2368f598da9
Jose Pereira <josep@dei.uc.pt>
2018-10-09 09:15:45 +0100

    Changing SecurePOC to use Logger

--- development/actuators/demo-system-service/src/main/java/eubr/atmosphere/tma/actuator/examples/SecurePOC.java
+++ development/actuators/demo-system-service/src/main/java/eubr/atmosphere/tma/actuator/examples/SecurePOC.java
@@ -1,5 +1,7 @@
 package eubr.atmosphere.tma.actuator.examples;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,13 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/securePOC")
 public class SecurePOC {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SecurePOC.class);
+
     @PostMapping(path = "/act",
             headers = "Accept=" + MediaType.APPLICATION_JSON_VALUE,
             consumes = {MediaType.APPLICATION_JSON_VALUE},
             produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody
     ResponseEntity<DataTransferObject> act(@RequestBody String json) {
-        System.out.println(json);
+        LOGGER.info(json);
         return ResponseEntity.ok()
                 .header("Custom-Header", "foo")
                 .body(new DataTransferObject("TESTE body!"));
