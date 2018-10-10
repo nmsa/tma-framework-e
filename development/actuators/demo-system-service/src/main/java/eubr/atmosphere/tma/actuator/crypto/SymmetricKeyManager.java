ee60211721c35bcaadf42a78ebbd91e36154b597
Jose Pereira <josep@dei.uc.pt>
2018-10-10 15:24:30 +0100

    Adding the method to save the symmetric key in a file

--- development/actuators/demo-system-service/src/main/java/eubr/atmosphere/tma/actuator/crypto/SymmetricKeyManager.java
+++ development/actuators/demo-system-service/src/main/java/eubr/atmosphere/tma/actuator/crypto/SymmetricKeyManager.java
@@ -1,8 +1,13 @@
 package eubr.atmosphere.tma.actuator.crypto;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -17,19 +22,30 @@ public class SymmetricKeyManager {
     public SymmetricKeyManager(String secret, int length, String algorithm)
             throws UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException {
         byte[] key = new byte[length];
-        key = fixSecret(secret, length);
+
+        String fixedSecret = fixSecret(secret, length);
+        key = fixedSecret.getBytes("UTF-8");
         this.secretKey = new SecretKeySpec(key, algorithm);
         this.cipher = Cipher.getInstance(algorithm);
+        try {
+            this.saveSecretKey(fixedSecret.toCharArray());
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private byte[] fixSecret(String s, int length) throws UnsupportedEncodingException {
+    private String fixSecret(String s, int length) throws UnsupportedEncodingException {
         if (s.length() < length) {
             int missingLength = length - s.length();
             for (int i = 0; i < missingLength; i++) {
                 s += " ";
             }
         }
-        return s.substring(0, length).getBytes("UTF-8");
+        return s.substring(0, length);
     }
 
     public byte[] encrypt(String message)
@@ -44,5 +60,20 @@ public class SymmetricKeyManager {
         this.cipher.init(Cipher.DECRYPT_MODE, this.secretKey);
         byte[] output = this.cipher.doFinal(message);
         return output;
+    }
+
+    public void saveSecretKey(char[] password) throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
+        /* save the secret key in a file */
+        byte[] key = this.secretKey.getEncoded();
+        FileOutputStream keyfos;
+        try {
+            keyfos = new FileOutputStream("/home/virt-atm/Documents/symmetricKey");
+            keyfos.write(key);
+            keyfos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
