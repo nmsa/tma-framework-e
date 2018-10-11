c288ad18198d975804b02289e761cbfa3cb3a4a2
Jose Pereira <josep@dei.uc.pt>
2018-10-11 14:11:25 +0100

    Removing unnecessary code

--- development/actuators/demo-system-service/src/main/java/eubr/atmosphere/tma/actuator/filters/DecryptFilter.java
+++ development/actuators/demo-system-service/src/main/java/eubr/atmosphere/tma/actuator/filters/DecryptFilter.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.security.InvalidKeyException;
 import java.security.KeyFactory;
@@ -21,10 +20,7 @@ import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -40,7 +36,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import eubr.atmosphere.tma.actuator.crypto.SymmetricKeyManager;
 import eubr.atmosphere.tma.actuator.wrappers.HttpServletRequestWritableWrapper;
 import eubr.atmosphere.tma.actuator.wrappers.HttpServletResponseCopier;
 
@@ -103,62 +98,14 @@ public class DecryptFilter implements Filter {
     private void encryptResponse(ServletResponse servletResponse, PrivateKey privateKey,
             String plainResponse) throws InvalidKeyException, IOException, NoSuchAlgorithmException, SignatureException {
 
-        PublicKey publicKeyActuator = getPublicKey("/home/virt-atm/Documents/pub-key-again");
-
         byte[] signedResponseByteArray = sign(plainResponse, privateKey);
         String signedResponse = Base64.getEncoder().encodeToString(signedResponseByteArray);
         LOGGER.info("signedResponse: " + signedResponse);
-
-        byte[] encryptedData = getEncryptedData(signedResponse);
-        byte[] decryptedDataByteArray = getDecryptedData(encryptedData);
-
-        LOGGER.info("MAIS UM TESTE: {}", verify(plainResponse, new String(decryptedDataByteArray), publicKeyActuator));
-        LOGGER.info("encryptedData: {}", new String(encryptedData));
-        LOGGER.info("decryptedDataByteArray: " + new String(decryptedDataByteArray));
-
-        LOGGER.info("encryptedData.length: {}", encryptedData.length);
-        LOGGER.info("(new String(encryptedData)).length(): {}", (new String(encryptedData)).length());
 
         PublicKey publicKeyExecutor = getPublicKey("/home/virt-atm/Documents/pub-key-execute");
         servletResponse.getWriter().write(Base64.getEncoder().encodeToString(encrypt(plainResponse.getBytes(), publicKeyExecutor)));
         servletResponse.getWriter().write("\n");
         servletResponse.getWriter().write(signedResponse);
-    }
-
-    public byte[] getEncryptedData(String message) {
-        SymmetricKeyManager skm;
-        byte[] output = null;
-        try {
-            skm = new SymmetricKeyManager(SECRET, 16, SYMMETRIC_KEY_ALGORITHM);
-            output = skm.encrypt(message);
-        } catch (UnsupportedEncodingException | NoSuchAlgorithmException | NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
-        }
-        return output;
-    }
-
-    public byte[] getDecryptedData(byte[] message) {
-        SymmetricKeyManager skm;
-        byte[] output = null;
-        try {
-            skm = new SymmetricKeyManager(SECRET, 16, "AES");
-            output = skm.decrypt(message);
-        } catch (UnsupportedEncodingException | NoSuchAlgorithmException | NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
-        }
-        return output;
     }
 
     //The method that signs the data using the private key that is stored in keyFile path
@@ -257,7 +204,7 @@ public class DecryptFilter implements Filter {
         return null;
     }
 
-    public static byte[] decrypt(byte[] text, PrivateKey key) {
+    private static byte[] decrypt(byte[] text, PrivateKey key) {
         byte[] dectyptedText = null;
         try {
             // get an RSA cipher object and print the provider
@@ -273,7 +220,7 @@ public class DecryptFilter implements Filter {
          return dectyptedText;
     }
 
-    public static byte[] encrypt(byte[] text, PublicKey key) {
+    private static byte[] encrypt(byte[] text, PublicKey key) {
         byte[] cipherText = null;
         try {
             // get an RSA cipher object and print the provider
