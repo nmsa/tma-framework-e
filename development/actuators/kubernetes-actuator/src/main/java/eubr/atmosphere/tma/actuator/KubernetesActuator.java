239df63aff45fd9870ba771e8391ba8d73b439e2
Jose Pereira <josep@dei.uc.pt>
2018-10-18 08:44:06 +0100

    Refactoring (moving class to another namespace)

--- development/actuators/kubernetes-actuator/src/main/java/eubr/atmosphere/tma/actuator/KubernetesActuator.java
+++ development/actuators/kubernetes-actuator/src/main/java/eubr/atmosphere/tma/actuator/KubernetesActuator.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import eubr.atmosphere.tma.actuator.examples.Configuration;
 import eubr.atmosphere.tma.actuator.services.RestServices;
 
 @RestController
--- development/actuators/kubernetes-actuator/src/main/java/eubr/atmosphere/tma/actuator/services/RestServices.java
+++ development/actuators/kubernetes-actuator/src/main/java/eubr/atmosphere/tma/actuator/services/RestServices.java
@@ -11,7 +11,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import eubr.atmosphere.tma.actuator.examples.Configuration;
+import eubr.atmosphere.tma.actuator.Configuration;
 
 public class RestServices {
 
