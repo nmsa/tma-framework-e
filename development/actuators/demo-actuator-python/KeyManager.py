5f305a467ff8dfe3191336d98dd4a74da0371a1b
Rui Silva <rslv560@gmail.com>
2019-04-14 10:10:48 +0100

    Adding Demo Actuator Python

---
+++ development/actuators/demo-actuator-python/KeyManager.py
@@ -0,0 +1,66 @@
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA
+import random
+import os
+import errno
+
+class KeyManager:
+	def __init__(self, algorithm = "RSA", code = "UTF-8", signature_algorithm = "SHA1withRSA"):
+		self.algorithm = algorithm
+		self.code = code
+		self.signature_algorithm = signature_algorithm
+
+	def decrypt(text, private_key_string):
+		# import private key to an RSA object
+		private_key = RSA.importKey(private_key_string)
+
+		# decrypt the text with the private key
+		decryptedText = private_key.decrypt(text)
+		return decryptedText
+
+	def encrypt(text, public_key_string):
+		# import public key to an RSA object
+		public_key = RSA.importKey(public_key_string)
+
+		# encrypt the plain text using the public key
+		encrypted = public_key.encrypt(text,random.randint(1,101))
+		return encryptedText
+
+	def getPrivateKey(filenameprivatekey):
+		# read private key from file
+		try:
+			privKey = open(filenameprivatekey,"r").read()
+		    return privKey
+		except EnvironmentError as e:
+			print(os.strerror(e.errno))
+			return None
+
+	def getPublicKey(filenamepublickey):
+		# read public key from file
+		try:
+			pubkey = open(filenamepublickey,"r").read()
+			return pubkey
+		except EnvironmentError as e:
+			print(os.strerror(e.errno))
+			return None
+
+	# The method that signs the data using the private key that is stored in keyFile path
+	def sign(data,keyFile):
+		privateSignature = RSA.importKey(keyFile)
+		h = SHA.new(data)
+		signer = PKCS1_v1_5.new(privateSignature)
+		signature = signer.sign(h)
+		return signature
+
+	def verfify (plainText,signature,publicKey):
+		publicSignature = RSA.importKey(publicKey)
+		h = SHA.new(plainText)
+		verifier = PKCS1_v1_5.new(publicSignature)
+		
+		if verifier.verify(h,signature):
+			message = "The signature is authentic"
+		else:
+			message = "The signature is not authentic"
+
+		return message
---
+++ development/actuators/demo-actuator-python/demo-actuator.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from flask import request
+import json
+import os
+import logging
+import logging.config
+from ActuatorPayload import ActuatorPayload
+
+k8sActuator = Flask(__name__)
+
+logger = logging.getLogger(__name__)
+logger.info('Starting Demo Actuator')
+
+@k8sActuator.route('/act', methods=['POST'])
+def process_message():
+  # load json file
+  input = request.get_json(force=True)
+  payload = ActuatorPayload(input)
+  return executeaction(payload.action)
+
+
+# Execute the action
+def executeaction(action):
+  logger.info('Action: %s', action)
+  switcher = {
+    "scale": "action " + action,
+    "email": "action " + action
+  }
+  return switcher.get(action, "Not defined action: " + action)
+  
+
+ # load logging configuration file
+def setup_logging(default_path='logging.json', env_key='LOG_CFG'):
+  path = default_path
+  value = os.getenv(env_key, None)
+  if value:
+     path = value
+  if os.path.exists(path):
+     with open(path, 'rt') as f:
+         config = json.load(f)
+     logging.config.dictConfig(config)
+  else:
+     logging.basicConfig(level=logging.DEBUG)
+
+
+if __name__ == '__main__':
+     setup_logging()
+     logger = logging.getLogger(__name__)
+     logger.info('Initializing  Demo Actuator')
+     k8sActuator.run(debug='True', host='0.0.0.0', port=5000)
