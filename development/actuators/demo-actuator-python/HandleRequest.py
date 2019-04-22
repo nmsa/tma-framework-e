4812bc0bc7cbcde227e9e7a40f5f67bdd851ee79
Rui Silva <rslv560@gmail.com>
2019-04-22 08:49:09 +0100

    Adding HandleRequest code

---
+++ development/actuators/demo-actuator-python/HandleRequest.py
@@ -0,0 +1,26 @@
+from KeyManager import KeyManager
+import base64
+
+class HandleRequest:
+
+	def encryptResponse(self, response, privateKey, plainResponse):
+
+		keymanager = KeyManager()
+		signedResponse = keymanager.sign(response,privateKey)
+		signedResponseEncoded = base64.b64encode(signedResponse)
+
+		publicKeyExecutorPath = "keys/pub-key-executor.pem"
+		publicKeyExecutor = keymanager.getPublicKey(publicKeyExecutorPath)
+		encryptedMessage = keymanager.encrypt(message,publicKeyExecutor)
+		response = base64.b64encode(str(encryptedMessage))
+		response = response + "\n"
+		response = response + signedResponseEncoded
+
+	def processRequest(self, request, response):
+
+		# TODO: Handle Requests where the key is not valid
+		privateKeyPath = "keys/private_key.pem" 
+		keymanager = KeyManager()
+		privateKey = keymanager.getPrivateKey(privateKeyPath)
+		decryptData = keymanager.decrypt(request, privateKey)
+		encryptResponse(response, privateKey, plainResponse)
