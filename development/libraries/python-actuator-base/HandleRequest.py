e9ff8ef6391409c7c16806049da0e18737656393
Jodao <49023295+Jodao@users.noreply.github.com>
2022-04-08 00:26:50 +0100

    Fixed communication between actuator and execue
    Made the following changes, which fixed communication issues and now allow using the current folder as a package:
    
    -Added a init file to allow using the folder as a package
    -Fixed HandleRequest and KeyManager code to allow proper communication with TMA's execute component

--- development/libraries/python-actuator-base/HandleRequest.py
+++ development/libraries/python-actuator-base/HandleRequest.py
@@ -4,32 +4,28 @@ import json
 from .ActuatorPayload import ActuatorPayload
 
 class HandleRequest:
-
-	def generateResponse(self, plainResponse):
-
-		keymanager = KeyManager()
-		privateKeyPath = "keys/priv-key-actuator"
-		privateKey = keymanager.getPrivateKey(privateKeyPath)
-
-		signedResponse = keymanager.sign(plainResponse,privateKey)
-		signedResponseEncoded = base64.b64encode(signedResponse)
-
-		publicKeyExecutorPath = "keys/pub-key-executor"
-		publicKeyExecutor = keymanager.getPublicKey(publicKeyExecutorPath)
-		encryptedMessage = keymanager.encrypt(plainResponse,publicKeyExecutor)
-		encryptedMessageBase = base64.b64encode(encryptedMessage)
-		response = str(encryptedMessageBase) + '\n' + str(signedResponseEncoded)
-		responseBytes = response.encode()
-		return responseBytes
-
-	def processRequest(self, request):
-
-		# TODO: Handle Requests where the key is not valid
-
-		privateKeyPath = "keys/priv-key-actuator"
-		keymanager = KeyManager()
-		privateKey = keymanager.getPrivateKey(privateKeyPath)
-		decryptData = keymanager.decrypt(request, privateKey)
-		input = json.loads(decryptData)
-		payload = ActuatorPayload(input)
-		return payload
+    def generateResponse(self, plainResponse):
+        keymanager = KeyManager()
+        privateKeyPath = "keys/priv-key-actuator"
+        privateKey = keymanager.getPrivateKey(privateKeyPath)
+        
+        signedResponseBytes = keymanager.sign(plainResponse,privateKey)
+        signedResponseBase64 = base64.b64encode(signedResponseBytes)
+        
+        publicKeyExecutorPath = "keys/pub-key-executor"
+        publicKeyExecutor = keymanager.getPublicKey(publicKeyExecutorPath)
+        encryptedMessageBytes = keymanager.encrypt(plainResponse,publicKeyExecutor)
+        encryptedMessageBase64 = base64.b64encode(encryptedMessageBytes)
+        responseBytes = encryptedMessageBase64 + '\n'.encode() + signedResponseBase64
+        
+        return responseBytes
+    
+    def processRequest(self, request):
+        # TODO: Handle Requests where the key is not valid
+        privateKeyPath = "keys/priv-key-actuator"
+        keymanager = KeyManager()
+        privateKey = keymanager.getPrivateKey(privateKeyPath)
+        decryptData = keymanager.decrypt(request, privateKey)
+        input = json.loads(decryptData)
+        payload = ActuatorPayload(input)
+        return payload
